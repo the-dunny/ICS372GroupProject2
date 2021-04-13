3e782e25f45f782977839d03f7d8b55a8e473241
the-dunny <jordandodd8184@gmail.com>
2021-04-13 12:11:05 -0500

    added reuqired methods for PropertyChangeListener

--- src/timer/Timer2.java
+++ src/timer/Timer2.java
@@ -3,10 +3,12 @@ package timer;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 
+import events.TimerRanOutEvent;
+import events.TimerTickedEvent;
 public class Timer2 implements PropertyChangeListener {
 	//instance variables are: an amount of time and a client
 	int timeRemaining;
-	Notifiable observerClient;
+	Notifiable client;
 	
 	/**
 	 * This constructor will initialize the instance variables of the timer,
@@ -17,23 +19,42 @@ public class Timer2 implements PropertyChangeListener {
 	 */
 	public Timer2(int initialTime, Notifiable newClient) {
 		this.timeRemaining = initialTime;
-		this.observerClient = newClient;
+		this.client = newClient;
 		Clock2.instance().addPropertyChangeListener(this);
 	}
 	
 	/**
-	 * Method to top off timer
+	 * Method to top off timer TODO don't know if we will need this??
 	 * @param additionalTime
 	 */
 	public void addTimeToTimer(int additionalTime) {
 		this.timeRemaining += additionalTime;
 	}
 	
+	/**
+	 * Method to get the current amount of time remaining
+	 * @return int - time remaining on the timer
+	 */
+	public int getRemainingTime() {
+		return this.timeRemaining;
+	}
 	
+	/**
+	 * This method will hand the propertyChange that takes place when the 
+	 * clock ticks off a second.  It will update and if there is not seconds
+	 * left on timer.  client should handle a TimerRanOutEvent
+	 * 
+	 */
 	@Override
 	public void propertyChange(PropertyChangeEvent arg0) {
-		// TODO Auto-generated method stub
-		
+		// if the timeRemaining less 1 leaves timer less than 0 - handle a TimeRanOutEvent
+		if((timeRemaining - 1) <= 0) {
+			client.handleEvent(TimerRanOutEvent.instance());
+			Clock.instance().removePropertyChangeListener(this);
+		}else {
+			//else update the timer
+			client.handleEvent(new TimerTickedEvent(timeRemaining));
+		}
 	}
 	
 
