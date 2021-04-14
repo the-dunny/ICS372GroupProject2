8ec27c3d44766cab984f54f01203d41d24f4a712
the-dunny <jordandodd8184@gmail.com>
2021-04-13 20:01:22 -0500

    deleted buttons and package -- got timer stuff dialed in

--- src/buttons/Zone1Button.java
+++
@@ -1,6 +0,0 @@
-package buttons;
-
-//TODO - adding appropriate methods for zone1 button
-public class Zone1Button {
-
-}
--- src/buttons/Zone3Button.java
+++
@@ -1,6 +0,0 @@
-package buttons;
-
-// TODO - adding appropriate methods for zone3 button
-public class Zone3Button {
-
-}
